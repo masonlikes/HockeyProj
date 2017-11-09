1afddd71c6472e0317611e970f91b318a5ffebb2
Mason Lawrence <lawrencemason00@gmail.com>
2017-11-09 10:54:13 -0700

    Initial Commit

---
+++ Hockey Project/Hockey Project/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Hockey Project
+//
+//  Created by Mason Lawrence on 11/9/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
